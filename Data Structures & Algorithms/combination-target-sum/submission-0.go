69391bbd81b829a4443885c1af656f1ab701d38c
Nguyễn Hoàng Anh <155830542+hoanganh0705@users.noreply.github.com>
2026-04-10 14:52:41 +0700

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.go
@@ -0,0 +1,29 @@
+func combinationSum(nums []int, target int) [][]int {
+    var res [][]int
+	var path []int
+
+	var backtrack func(start int, target int)
+	backtrack = func(start int, target int) {
+		if target == 0 {
+			temp := make([]int, len(path))
+			copy (temp, path)
+			res = res.append(res, path)
+			return
+		}
+
+		for i := start; i< len(nums); i++ {
+			if nums[i] > target {
+				return
+			}
+
+			path = append(path, nums[i])
+
+			backtrack(i+1, target-nums[i])
+
+			path = path[:len(path) - 1]
+		}
+	}
+
+	backtrack(0, target)
+	return res
+}
