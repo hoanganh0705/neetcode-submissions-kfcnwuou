de605decda900952fae9b118d1a7540cdebbbe0a
Nguyễn Hoàng Anh <155830542+hoanganh0705@users.noreply.github.com>
2026-06-15 09:42:23 +0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.go
@@ -0,0 +1,20 @@
+func groupAnagrams(strs []string) [][]string {
+	group := make(group[[26]int][]string)
+
+	for _, word := range strs {
+		var count [26] int
+		for char in range word {
+			count[char-'a']++
+		}
+
+		group[count] = append(group[count], word)
+	}
+
+	result = make([][]string, 0, len(group))
+
+	for _, group := range groups {
+    	result = append(result, group)
+    }
+
+	return result
+}
