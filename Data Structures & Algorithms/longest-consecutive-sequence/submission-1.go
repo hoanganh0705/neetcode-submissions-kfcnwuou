6fb6a93e796d5efea89b54abfab3028aae9068c2
Nguyễn Hoàng Anh <155830542+hoanganh0705@users.noreply.github.com>
2026-07-01 05:06:28 +0100

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.go
@@ -0,0 +1,34 @@
+func longestConsecutive(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	// hashset
+	set := make(map[int]bool)
+	for _, num := range nums {
+		set[num] = true
+	}
+
+	longest := 0
+
+	for _, num := range nums {
+		if set[num-1] {
+			continue
+		}
+
+		// num is the first number of consecutive sequence
+		length := 1
+		current := num
+
+		for set[current+1] {
+			current++
+			length++
+		}
+
+		if length > longest {
+			longest = length
+		}
+	}
+
+	return longest
+}
